5f94732a142182232324363f50899e8b7dc58654
jdg <jiaodaguan@gmail.com>
2023-03-30 10:03:46 +0800

    feat

--- src/main/kotlin/com/point18/slg2d/avatar/config/PropertiesConfig.kt
+++ src/main/kotlin/com/point18/slg2d/avatar/config/PropertiesConfig.kt
@@ -4,6 +4,7 @@ import com.point18.slg2d.avatar.constg.MockSceneType
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConfigurationPropertiesBinding
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.PropertySource
 import org.springframework.core.convert.converter.Converter
@@ -27,7 +28,13 @@ class PropertiesConfig {
     }
 }
 
-@PropertySource(value = ["file:\${user.dir}/config/avatar.properties"], encoding = "UTF-8")
+@Configuration
+@PropertySource(
+    //value = ["classpath:avatar.properties"],
+    value = ["file:\${user.dir}/config/avatar.properties"],
+    encoding = "UTF-8",
+    ignoreResourceNotFound = false
+)
 @ConfigurationProperties(prefix = "slg2d.server", ignoreInvalidFields = false)
 class ServerProperties {
     var clusterId: Long? = null
@@ -37,7 +44,12 @@ class ServerProperties {
     var partition: Int? = null
 }
 
-@PropertySource(value = ["file:\${user.dir}/config/avatar.properties"], encoding = "UTF-8")
+@Configuration
+@PropertySource(
+    value = ["file:\${user.dir}/config/avatar.properties"],
+    encoding = "UTF-8",
+    ignoreResourceNotFound = false
+)
 @ConfigurationProperties(prefix = "slg2d.avatar", ignoreInvalidFields = false)
 class AvatarProperties {
     var nameprefix: String? = null
