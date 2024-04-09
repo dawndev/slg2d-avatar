628170e9645f64650b6e45f114b152fcd71719fc
bezy <monokai@qq.com>
2024-04-09 13:56:56 +0800

    feat: 一些扩展方法

--- src/main/kotlin/com/point18/slg2d/avatar/extension/Extends.kt
+++ src/main/kotlin/com/point18/slg2d/avatar/extension/Extends.kt
@@ -1,7 +1,27 @@
 package com.point18.slg2d.avatar.extension
 
+import akka.actor.AbstractActor
 import akka.actor.ActorRef
+import akka.actor.ActorSystem
+import akka.actor.Props
 
 fun ActorRef.tellWithNoSender(any: Any) {
     this@tellWithNoSender.tell(any, ActorRef.noSender())
+}
+
+fun ActorSystem.createActor(props: Props, name: String): ActorRef {
+    return this.actorOf(props, name)
+}
+
+fun ActorRef.send(message: Any, sender: ActorRef = ActorRef.noSender()) {
+    this.tell(message, sender)
+}
+
+inline fun <reified T : AbstractActor> propsOf(vararg args: Any?): Props {
+    return Props.create(T::class.java, *args)
+}
+
+// "hello world" --> "HelloWorld"
+fun String.toCamelCase(): String {
+    return this.split(" ").joinToString("") { it.capitalize() }
 }
